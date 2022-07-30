fc5dade129fb12671a58d2aa8785e7edc0651380
lorenamucheniski <lomucheniski25@gmail.com>
2022-07-30 14:55:32 -0300

    dobro,triplo e raiz quadrada

---
+++ ex04.py
@@ -0,0 +1,3 @@
+num = int (input('Digite um número: '))
+print('O dobro de {} é igual a {}.'.format(num, (num*2)))
+print('A triplo de {} é igual a {}. \nA raiz quadrada de {} é igual a {}.'.format(num, (num*3), num, (num*(1/2))))
