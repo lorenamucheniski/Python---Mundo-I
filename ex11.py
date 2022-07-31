15e032d2ace745f12fd35b8c23a05e6a648ce69f
lorenamucheniski <lomucheniski25@gmail.com>
2022-07-31 14:19:54 -0300

    quebrando um número

---
+++ ex11.py
@@ -0,0 +1,3 @@
+from math import trunc
+num = float(input('Digite um número real para ver sua porção inteira:'))
+print('O número digitado foi {} e a sua porção inteira é {}.'.format(num, trunc(num)))
